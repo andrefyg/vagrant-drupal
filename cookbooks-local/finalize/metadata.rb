e7eef5b2a62aafd295017e6c90a3c37a2c1d38f9
ksorokin <k.n.sorokin@gmail.com>
2013-08-07 00:25:57 +0300

    added varnish and solr setup to finalize::drupal

--- cookbooks-local/finalize/metadata.rb
+++ cookbooks-local/finalize/metadata.rb
@@ -3,7 +3,11 @@ maintainer_email "k.n.sorokin@gmail.com"
 license          "Apache 2.0"
 description      "Installs/Configures finalize"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+
 version          "0.1.0"
+
 depends          "php"
 depends          "apache2"
 depends          "drush"
+depends          "varnish"
+depends          "solr"
